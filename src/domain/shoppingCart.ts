9502d6dff4ee89ae58ca841b3fa81998418244cb
Jonas <boone.jonas@outlook.be>
2022-02-17 09:05:16 +0100

    refactor(ShoppingCart): cleanup

--- src/domain/shoppingCart.ts
+++ src/domain/shoppingCart.ts
@@ -1,21 +1,9 @@
-/* eslint-disable max-len */
-import Product from './product';
-
-export interface CartItem {
-  product: Product;
-  quantity: number;
-}
-
-export interface Cart {
-  items: CartItem[];
-}
-
-export function calculateTotalCostPerCartItem(cartItem: CartItem) {
+// TODO: fix this typing later based on generated graphQL types
+export function calculateTotalCostPerCartItem(cartItem: any) {
   return parseInt((cartItem.quantity * cartItem.product.price).toFixed(2), 10);
 }
 
-export function calculateTotalCartCost(cartItems: CartItem[]) {
+export function calculateTotalCartCost(cartItems: any[]) {
   const totalPerProductArray = cartItems.map((item) => calculateTotalCostPerCartItem(item));
-  return totalPerProductArray.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
-    .toFixed(2);
+  return totalPerProductArray.reduce((previousValue, currentValue) => previousValue + currentValue, 0).toFixed(2);
 }
