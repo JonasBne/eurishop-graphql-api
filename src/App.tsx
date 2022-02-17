dae5383fa900edc9fb4b12dcc604d451e6f253a6
Jonas <boone.jonas@outlook.be>
2022-02-17 09:00:12 +0100

    refactor(App): removed Query client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { Toaster } from 'react-hot-toast';
 import theme from './theme/theme';
@@ -12,25 +10,20 @@ import ProductList from './views/ProductList/ProductList';
 import GlobalStyle from './theme/globalStyle';
 import ProductAdd from './views/ProductDetail/ProductAdd';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Navbar />
-        <Toaster position="top-right" />
-        <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="products/admin" element={<ProductList />} />
-          <Route path="products/:productId/edit" element={<ProductEdit />} />
-          <Route path="products/new" element={<ProductAdd />} />
-        </Routes>
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Navbar />
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="products/admin" element={<ProductList />} />
+        <Route path="products/:productId/edit" element={<ProductEdit />} />
+        <Route path="products/new" element={<ProductAdd />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
