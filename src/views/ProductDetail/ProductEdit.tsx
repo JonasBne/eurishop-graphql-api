79f78a141c68cf350295e919248adfd8295046c4
Jonas <boone.jonas@outlook.be>
2022-02-23 09:13:01 +0100

    refactor(ProductEdit): products can now be edited

--- src/views/ProductDetail/ProductEdit.tsx
+++ src/views/ProductDetail/ProductEdit.tsx
@@ -1,13 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import ErrorModal from '../../components/ErrorModal/ErrorModal';
 import ProductForm, { ProductFormValues } from './ProductForm';
 import toasts from '../../components/toasts';
-import { GetSingleProductQuery } from '../../graphql/types';
-import { GET_SINGLE_PRODUCT } from '../../graphql/queries';
+import {
+  AddOrUpdateProductMutation,
+  AddOrUpdateProductMutationVariables,
+  GetSingleProductQuery,
+  ProductInput,
+} from '../../graphql/types';
+import { GET_SINGLE_PRODUCT, UPDATE_PRODUCT } from '../../graphql/queries';
+import { GET_PRODUCTS } from '../../graphql/queries/ProductList';
 
 function ProductEdit() {
   const { succesToast, failToast } = toasts();
@@ -22,6 +27,17 @@ function ProductEdit() {
 
   const product = data?.product;
 
+  const [updateProduct, { error: putError, data: puttedData }] = useMutation<
+    AddOrUpdateProductMutation,
+    AddOrUpdateProductMutationVariables
+  >(UPDATE_PRODUCT, {
+    refetchQueries: [
+      {
+        query: GET_PRODUCTS,
+      },
+    ],
+  });
+
   const gridTemplateAreas = `
   "title sku"
   "basePrice price"
@@ -29,30 +45,33 @@ function ProductEdit() {
   "desc desc"
   `;
 
-  // useEffect(() => {
-  //   if (putError) {
-  //     failToast(putError);
-  //   }
-  //   if (puttedData) {
-  //     succesToast(`Item with id ${puttedData.id} updated!`);
-  //     navigate('/products/admin');
-  //   }
-  // }, [putError, puttedData]);
+  useEffect(() => {
+    if (putError) {
+      failToast(putError);
+    }
+    if (puttedData) {
+      succesToast(`Item with id ${puttedData.addOrUpdateProduct?.product?.id} updated!`);
+      navigate('/products/admin');
+    }
+  }, [putError, puttedData]);
 
   const handleCancel = () => {
     navigate('/products/admin');
   };
 
-  // TODO: fix types here
   const handleSubmit = (formValues: ProductFormValues) => {
-    const item: any = {
+    const item: ProductInput = {
       ...formValues,
       id: +formValues.id,
       basePrice: +formValues.basePrice,
       price: +formValues.price,
     };
 
-    console.log(item);
+    updateProduct({
+      variables: {
+        product: item,
+      },
+    });
   };
 
   return (
